cc8e64541c47cb5f9d3cdf87f2af0141e3721ef8
Sergey Barbolin <sergeybarbolin25@gmail.com>
2019-10-18 13:10:18 +0300

    fix "if" & await copyfile, mkdir

--- sortFilesPromise.js
+++ sortFilesPromise.js
@@ -7,45 +7,24 @@ const newDir = './newfiles';
 const readDir = (base, level) => {
   return fs.readdir(base)
     .then(files => {
-      files.forEach(item => {
+      files.forEach(async (item) => {
         const localBase = path.join(base, item);
-  
-        fs.stat(localBase)
-          .then(state => state.isDirectory())
-          .then(directory => {
-            if (directory) {
-              readDir(localBase, level + 1);
-            } else {
-              return item;
-            }
-          })
-          .then(fileName => {
-            const folderName = path.join(newDir, item[0].toUpperCase());
-            const newPathFile = path.join(newDir, item[0].toUpperCase(), fileName);
+        const directory = await fs.stat(localBase).then(state => state.isDirectory())
 
-            fs.mkdir(folderName, err => {
-              if (err && err.code !== 'EEXIST') {
-                console.log(err.message);
-                return;
-              }
-              fs.link(localBase, newPathFile, err => {
-                if (err) {
-                  console.error(`Файл ${newPathFile} уже существует.`);
-                  return;
-                }
-      
-                console.log(`Copied ${item} to ${folderName}`);
-              });
-    
-            });
-          })
-          .catch(err => {
-            console.error(err.message);
-            return;
-          });
+        if (directory) {
+          readDir(localBase, level + 1);
+        } else {
+          const folderName = path.join(newDir, item[0].toUpperCase());
+          const newPathFile = path.join(newDir, item[0].toUpperCase(), item);
+          
+          await fs.mkdir(folderName).catch(err => err.message);
+
+          fs.copyFile(localBase, newPathFile)
+            .then(() => { console.log(`Copied ${item} to ${folderName}`) })  
+            .catch(err => { console.log(err.message) });
+        }
       })
     })
-    
     .catch(err => {
       console.error(err.message);
       return;
