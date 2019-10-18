96a5acbe2e36a97ef13713eaf804a898f935e35a
Sergey Barbolin <sergeybarbolin25@gmail.com>
2019-10-18 14:48:11 +0300

    remove moment, fix res.end

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 require('dotenv').config();
 const http = require('http');
-const moment = require('moment');
 
 http.createServer(function(req, res) {
+    console.log('http://localhost:3000/');
+
     if (req.method === 'GET' && req.url === '/') {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end('<h1>RUN</h1>');
         
-        const timeEnd = moment().add(parseInt(process.env.INTERVALEND), 'milliseconds');
-
+        const timeEnd = new Date().getTime() + parseInt(process.env.INTERVALEND);
         const interval = setInterval(() => {
-            const currentTime = moment().format('HH:mm:ss');
+            const currentDate = new Date();
 
-            console.log(currentTime);
-
-            if (currentTime > timeEnd.format('HH:mm:ss')) {
+            if (currentDate.getTime() > timeEnd) {
                 clearInterval(interval);
-                console.log(moment().format());
+                res.end(currentDate.toUTCString());
+                console.log('Complete! Date: ');
             }
+            console.log(currentDate.toUTCString());
+
         }, parseInt(process.env.INTERVAL));
-        
     }
 }).listen(3000);
